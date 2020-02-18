86927061abe641089cd260d2172db42ad2c6a43c
vbalasu <vbalasu@gmail.com>
2020-02-18 08:49:09 -0800

    Added PythonUDF.py

--- PythonUDF.py
+++ PythonUDF.py
@@ -1,2 +1,16 @@
-def test(*args):
+def base64encode(s):
+  import base64
+  return base64.b64encode(s)
+def base64decode(s):
+  import base64
+  return base64.b64decode(s)
+def sha256(s):
+  import hashlib
+  return hashlib.sha256(s).hexdigest()
+def sha512(s):
+  import hashlib
+  return hashlib.sha512(s).hexdigest()
+def mask(s):
+  return s[-4:].rjust(len(s), "*")
+def test(*args):  # Simply converts to uppercase, but illustrates that multiple string arguments are possible
     return ' '.join(list(args)).upper()
